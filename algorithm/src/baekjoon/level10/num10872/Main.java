7b3411e85b8d3191282fbdb37b44f4faa1555483
turong92 <rkclrhks33@gmail.com>
2021-07-09 20:41:58 +0900

    백준 단계별 level10 10872

---
+++ algorithm/src/baekjoon/level10/num10872/Main.java
@@ -0,0 +1,44 @@
+package baekjoon.level10.num10872;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(factorial(Integer.parseInt(br.readLine())));
+		
+		br.close();
+	}
+	
+	public static int factorial(int num) {
+		if(num <= 1) {
+			return 1;
+		}else {
+			return factorial(num - 1) * num;
+		}
+	}
+
+}
+
+/*
+문제
+0보다 크거나 같은 정수 N이 주어진다. 이때, N!을 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 정수 N(0 ≤ N ≤ 12)가 주어진다.
+
+출력
+첫째 줄에 N!을 출력한다.
+
+예제 입력 1 
+10
+예제 출력 1 
+3628800
+예제 입력 2 
+0
+예제 출력 2 
+1
+*/
